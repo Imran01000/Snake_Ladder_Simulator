397c8b11f393e41b7015e07b8942f3f7aa8de530
Imran01000 <imshaik01000@gmail.com>
2020-02-28 16:48:07 +0530

    ADDED:Added random function to get (1 to 6) number.

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,7 +1,17 @@
-
-public class Snake_Ladder {
+import java.util.*;
+public class Snake_Ladder 
+{
 	public static void main(String[] args)
 	{
+		Random r=new Random();
 		System.out.println("Lets play snake and ladder game ");
+		System.out.println("The player roll the die");
+		int random=r.nextInt(6);
+		int played=random+1;
+		System.out.println("Outcome of die is : "+played);
+	
+		
 	}
+		
+		
 }
