68d6b0effc4795bfe63f8f9ca9c6aeed7b89c337
Imran01000 <imshaik01000@gmail.com>
2020-02-28 17:34:40 +0530

    ADDED:Added feature to check the option for Noplay,ladderor snake.

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,15 +1,48 @@
 import java.util.*;
 public class Snake_Ladder 
 {
+	//Constant variables.
+	static int noPlay=0;
+	static int ladder=1;
+	static int snake=2;
+	
+	//Variables.
+	static int count1=0;
+	static int count2=0;
+	
+	//Array declaration.
+	static int arrayGame[]=new int[100];
+	
+	static Random r=new Random();
 	public static void main(String[] args)
 	{
-		Random r=new Random();
+		
 		System.out.println("Lets play snake and ladder game ");
 		System.out.println("The player roll the die");
 		int random=r.nextInt(6);
 		int played=random+1;
 		System.out.println("Outcome of die is : "+played);
-	
+		int i=0;
+			int randomCheck=r.nextInt(3);
+			if(randomCheck == noPlay)
+			{
+				System.out.println("Player stay in the same position");
+			}
+			else if(randomCheck == ladder)
+			{
+				i=i+played;
+				count1++;
+				System.out.println(i);
+				System.out.println("ladder "+count1);
+			}
+			else if(randomCheck == snake)
+			{
+				i=i-played;
+				count2++;
+				System.out.println(i);
+				System.out.println("Snake "+count2);
+			}
+			
 		
 	}
 		
