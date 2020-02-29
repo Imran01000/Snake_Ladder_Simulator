f0cb6c298d9227b4e13f3cd6e1d9c02bbeb44851
Imran01000 <imshaik01000@gmail.com>
2020-02-29 13:36:55 +0530

    ADDED:To check wheater player1 is won or player 2.

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -10,6 +10,12 @@ public class Snake_Ladder
 	static int count1=0;
 	static int count2=0;
 	static int position=0;
+	static int positionForPlayer1=0;
+	static int positionForPlayer2=0;
+	static int dieCountForPlayer1=0;
+	static int dieCountForPlayer2=0;
+	static int recentPosition=0;
+	
 	
 	//Getting dice value.
 	public static int getDice()
@@ -21,65 +27,95 @@ public class Snake_Ladder
 	
 	//Random object to generate random value.
 	static Random r=new Random();
+
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Lets play snake and ladder game ");
 		System.out.println("The player roll the die");
-		
 		//Repeating the loop till position is not reaching to hundred.
-		while(position < 100)
-		{	
-			int tempPosition=position;
-			int play=getDice();
-			System.out.println("Dice value come is : "+play);
-
-			//generating random value for an option.
-			int randomCheck=r.nextInt(3);
-			
-			//checking for an option.
-			switch(randomCheck)
+		while(true)
+		{
+			if(positionForPlayer1 < 100 && positionForPlayer2 < 100)
 			{
-			
-			case 0:
-				{
-					System.out.println("Player stay in the same position");
-					break;
-				}					
-			case 1:
-				{	
-					if( (position+play) > 100)
-					{
-						position=tempPosition;		
-					}
-					else
-					{
-						position=position+play;
-					}
-					count1++;
-					System.out.println("Ladder come and update postion is : "+position);
-					break;
-				}
-			case 2: 	
-				{
-					if( (position-play) < 0)
-					{
-						position=0;
-						System.out.println("Position is from start : "+position);
-						
-					}
-					else
-					{
-						position=position-play;
-						count2++;
-						System.out.println("Snake come and update position is : "+position);
-					}
-				
-					break;
-				}	
-			
+				dieCountForPlayer1++;
+				positionForPlayer1=playSnakeAndLadderGame(positionForPlayer1);
+				dieCountForPlayer2++;
+				positionForPlayer2=playSnakeAndLadderGame(positionForPlayer2);
+			}
+			else
+			{
+				break;
+			}
 			
 		}
+		if(positionForPlayer1==100)
+		{
+			System.out.println("Player 1 won.... you rolled the die for"+dieCountForPlayer1+"times");
+		}
+		else
+		{
+			System.out.println("Player 2 won.... you rolled the die for"+dieCountForPlayer2+"times");
+		}
 	}
-}
-}
+
+	private static int playSnakeAndLadderGame(int position) 
+	{
+		
+		int tempPosition=position;
+		int play=getDice();
+		System.out.println("Dice value come is : "+play);
+
+		//generating random value for an option.
+		int randomCheck=r.nextInt(3);
+		
+		//checking for an option.
+		switch(randomCheck)
+		{
+		
+		case 0:
+			{
+				recentPosition=position;
+				System.out.println("Player stay in the same position");
+				break;
+			}	
+			
+		case 1:
+			{	
+				if( (tempPosition+play) > 100)
+				{
+					position=tempPosition;
+					recentPosition=position;
+				}
+				else
+				{
+					tempPosition=tempPosition+play;
+					recentPosition=tempPosition;
+					System.out.println("Ladder come and update postion is : "+tempPosition);
+				}
+				break;
+			}
+		case 2: 	
+			{
+				if( (tempPosition-play) < 0)
+				{
+					tempPosition=0;
+					recentPosition=tempPosition;
+					System.out.println("Position is from start : "+tempPosition);
+					
+				}
+				else
+				{
+					tempPosition=tempPosition-play;
+					recentPosition=tempPosition;
+					System.out.println("Snake come and update position is : "+tempPosition);
+				}
+				break;
+			}
+			
+		}	
+		return recentPosition;
+		
+	}
+}		
+	
